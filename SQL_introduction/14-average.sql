23852cb17b495f2b015eccab955082f4ddbd0579
KlaudioEpir <klajd.epir@gmail.com>
2026-09-16 18:10:06 +0200

    task 16

--- SQL_introduction/14-average.sql
+++ SQL_introduction/14-average.sql
@@ -1,2 +1,2 @@
--- Computes the score average of all records in the table second_table
+-- Script to compute the average score of all records in the table second_table
 SELECT AVG(score) AS average FROM second_table;
