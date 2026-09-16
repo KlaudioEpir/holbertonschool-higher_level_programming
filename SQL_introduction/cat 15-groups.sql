2a43cac1721b0e8d58895529f8787f08d69a49fe
KlaudioEpir <klajd.epir@gmail.com>
2026-09-16 18:06:17 +0200

    task 15

--- SQL_introduction/cat 15-groups.sql
+++ SQL_introduction/cat 15-groups.sql
@@ -1,3 +1,5 @@
+-- Lists the number of records with the same score in the table second_table
+-- Results are grouped by score, ordered by the count of records descending
 SELECT score, COUNT(*) AS number
 FROM second_table
 GROUP BY score
