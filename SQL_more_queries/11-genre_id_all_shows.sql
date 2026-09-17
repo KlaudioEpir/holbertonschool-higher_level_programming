db1e330074b649161fcfaf407514c305c30a4370
KlaudioEpir <klajd.epir@gmail.com>
2026-09-17 18:17:50 +0200

    task 10

---
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,7 @@
+-- Lists all shows.
+-- Displays tv_shows.title and tv_show_genres.genre_id.
+-- Shows without a genre display.
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
